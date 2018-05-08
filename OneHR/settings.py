2552006f1ac3446a7d7fb8f37bc7323018b675ff
RickyWong1991 <sskysstar@gmail.com>
2018-05-08 08:19:54 +0800

    1
    Co-Authored-By: rickywong1991 <rickywong1991@users.noreply.github.com>

--- OneHR/settings.py
+++ OneHR/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'PyOneHR.apps.PyonehrConfig',
+    'rest_framework_swagger',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'OneHR.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -92,7 +91,6 @@ DATABASES = {
 
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -111,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -124,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
--- PyOneHR/models.py
+++ PyOneHR/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Employee(models.Model):
+    ID = models.UUIDField(default=uuid.uuid1(), max_length=50, primary_key=True)
+
+
+class SystemUsersEN(models.Model):
+    ID = models.CharField(max_length=50, default=uuid.uuid1())
+    EmployeeUID = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    UserName = models.CharField(max_length=100, primary_key=True, null=False)
