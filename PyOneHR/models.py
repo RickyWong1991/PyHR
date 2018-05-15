ded0765163cad5fd6ab567b0742edd0af7927c02
RickyWong1991 <sskysstar@gmail.com>
2018-05-15 20:23:59 +0800

    create

--- PyOneHR/models.py
+++ PyOneHR/models.py
@@ -8,6 +8,6 @@ class Employee(models.Model):
 
 
 class SystemUsersEN(models.Model):
-    ID = models.CharField(max_length=50, default=uuid.uuid1())
+    ID = models.UUIDField(max_length=50, default=uuid.uuid1())
     EmployeeUID = models.ForeignKey(Employee, on_delete=models.CASCADE)
     UserName = models.CharField(max_length=100, primary_key=True, null=False)
